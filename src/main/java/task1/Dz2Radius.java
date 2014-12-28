9f5b10006570738192d2c487ceaf8daf51b52134
SvetlanaLu4 <SvetlanaLu4@yandex.ru>
2014-12-28 16:10:02 +0200

    Task1, Task2

--- src/main/java/task1/Dz2Radius.java
+++ src/main/java/task1/Dz2Radius.java
@@ -1,7 +1,12 @@
-package main.java.task1;
-
-/**
- * Created by Svetlana on 28.12.2014.
- */
 public class Dz2Radius {
+    public static void main (String[] args) {
+        int radius = 4;
+        int diameter = radius*2;
+        float circleLenth = 3.14F*diameter;
+        float square = 3.14F*(radius*radius);
+        System.out.println("Radius: "+ radius);
+        System.out.println("Diameter: "+ diameter);
+        System.out.println("Dlina Okruznosti: "+ circleLenth);
+        System.out.println("Square: "+ square);
+    }
 }
--- src/main/java/task2/Task2.java
+++ src/main/java/task2/Task2.java
@@ -1,7 +1,37 @@
-package main.java.task2;
-
-/**
- * Created by Svetlana on 26.12.2014.
- */
+package task2;
 public class Task2 {
+    public static void main(String[] args) {
+        int[] numbers = new int[]{7, 12, 34, 23, 22, 23, 45, 456, -6, 56, 56, 3434, 34, 57, 76, 456, 34, 24, 24, 56};
+        int sum = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            sum = sum + numbers[i];
+        }
+        int min = numbers[0];
+        for (int i = 0; i < numbers.length; i++) {
+            if (min > numbers[i]) {
+                min = numbers[i];
+            }
+        }
+        int max = numbers[0];
+        for (int i = 0; i < numbers.length; i++) {
+            if (max < numbers[i]) {
+                max = numbers[i];
+            }
+        }
+        System.out.println("Array(elementsCount=" + numbers.length + ", " + "elementsSum=" + sum + ", " + "minElement=" + min + ", " + "maxElement=" + max + ")");
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(i + ": " + numbers[i]);
+        }
+    }
 }
+
+
+     //Пример вывода:
+     //Array(elementsCount=20, elementsSum=25387, minElement=2, maxElement=2447)
+     //0: 432
+     //1: 23
+     //2: 2
+     //3: 16
+     //4: 447
+     //5: 113
+
